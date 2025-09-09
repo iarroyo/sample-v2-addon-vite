7e43dcee66957813d23c318b766dfdfe7636da9b
iarroyo <iarroyo@copyright.com>
2025-09-09 11:35:09 +0200

    Update tsconfig and template-registry

--- src/template-registry.ts
+++ src/template-registry.ts
@@ -2,7 +2,7 @@
 // Add all your components, helpers and modifiers to the template registry here, so apps don't have to do this.
 // See https://typed-ember.gitbook.io/glint/environments/ember/authoring-addons
 
-import type { Foo } from './components/foo';
+import type { Foo } from './components/foo.gts';
 import type { Sample } from './components/sample';
 
 export default interface Registry {
