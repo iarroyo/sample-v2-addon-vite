bb847d04df26cba0bfd0b324f158c7201cd5395b
iarroyo <iarroyo@copyright.com>
2025-09-11 16:22:09 +0200

    add ember-intl

--- unpublished-development-types/index.d.ts
+++ unpublished-development-types/index.d.ts
@@ -3,12 +3,15 @@
 
 import '@glint/environment-ember-loose';
 import '@glint/environment-ember-template-imports';
+import type EmberIntlRegistry from 'ember-intl/template-registry';
 
 // Uncomment if you need to support consuming projects in loose mode
 //
-// declare module '@glint/environment-ember-loose/registry' {
-//   export default interface Registry {
-//     // Add any registry entries from other addons here that your addon itself uses (in non-strict mode templates)
-//     // See https://typed-ember.gitbook.io/glint/using-glint/ember/using-addons
-//   }
-// }
+
+declare module '@glint/environment-ember-loose/registry' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  export default interface Registry
+    extends EmberIntlRegistry /* other addon registries */ {
+    // local entries
+  }
+}
